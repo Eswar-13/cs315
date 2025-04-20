63b2912699771122f4d7398d8e04a0e9e57d8c42
Eswar Sajja <eswarsajja13@gmail.com>
2025-04-20 12:15:03 +0530

    superuser access is given

--- search/admin.py
+++ search/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Station
+from .models import Schedule
+from .models import Train
 
-# Register your models here.
+
+admin.site.register(Station)
+admin.site.register(Schedule)
+admin.site.register(Train)
+
